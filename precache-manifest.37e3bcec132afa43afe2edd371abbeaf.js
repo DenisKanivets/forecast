c826f601bc722f8ed39b5098a646b467f116ceee
Denis Kanivets <denis.kanivets@ukr.net>
2019-03-22 10:53:56 +0200

    Updates

---
+++ precache-manifest.37e3bcec132afa43afe2edd371abbeaf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0a580239fe3d5607ccd4",
+    "url": "/forecast/static/js/runtime~main.1c70b54c.js"
+  },
+  {
+    "revision": "ce4d07c10e358318a60e",
+    "url": "/forecast/static/js/main.be4bfbeb.chunk.js"
+  },
+  {
+    "revision": "1b633df5c581867c1fd7",
+    "url": "/forecast/static/js/2.6e8f7c40.chunk.js"
+  },
+  {
+    "revision": "ce4d07c10e358318a60e",
+    "url": "/forecast/static/css/main.d4c122c2.chunk.css"
+  },
+  {
+    "revision": "5c7e18f3a41e481e3167d29f6d8933fd",
+    "url": "/forecast/index.html"
+  }
+];
